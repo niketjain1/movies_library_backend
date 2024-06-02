de36e80b878345e617875de76c578db2450c8c1c
niketjain1 <niketj2000@gmail.com>
2024-06-02 23:51:14 +0530

    🐛 FIX:  Added TypeOrmModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,14 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "./users.entity";
-import { UsersService } from "./users.service";
-import { UsersController } from "./users.controller";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users.entity';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { ListModule } from 'src/playlist/list.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    controllers: [UsersController],
-    providers: [UsersService]
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService, TypeOrmModule],
 })
-
 export class UsersModule {}
