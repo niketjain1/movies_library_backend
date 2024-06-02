e3497eb5fc8d1905458fdacf218e51d54643af29
niketjain1 <niketj2000@gmail.com>
2024-06-02 23:50:57 +0530

    👌 IMPROVE: edit the list creation and fetching functions

--- src/playlist/list.service.ts
+++ src/playlist/list.service.ts
@@ -5,6 +5,7 @@ import { MovieService } from '../movie/movie.service';
 import { List } from './list.enitity';
 import { Repository } from 'typeorm';
 import { User } from 'src/users/users.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class ListService {
@@ -12,15 +13,32 @@ export class ListService {
     @InjectRepository(List)
     private listRepository: Repository<List>,
     private movieService: MovieService,
+    private usersService: UsersService,
   ) {}
 
-  async createList(user: User, name: string, isPublic: boolean): Promise<List> {
-    const list = this.listRepository.create({ user, name, isPublic });
+  async createList(
+    userId: number,
+    name: string,
+    isPublic: boolean,
+  ): Promise<List> {
+    const user = await this.usersService.getUserById(userId);
+    const list = await this.listRepository.create({
+      name: name,
+      user: user,
+      isPublic: isPublic,
+    });
     return await this.listRepository.save(list);
   }
 
-  async addMovieToList(user: User, listId: number, movieTitle: string): Promise<List> {
-    const list = await this.listRepository.findOne({ where: { id: listId, user }, relations: ['movies'] });
+  async addMovieToList(
+    user: User,
+    listId: number,
+    movieTitle: string,
+  ): Promise<List> {
+    const list = await this.listRepository.findOne({
+      where: { id: listId, user },
+      relations: ['movies'],
+    });
 
     if (!list) {
       throw new NotFoundException(`List with id "${listId}" not found`);
@@ -32,12 +50,19 @@ export class ListService {
     return await this.listRepository.save(list);
   }
 
-  async getListsByUser(user: User): Promise<List[]> {
-    return await this.listRepository.find({ where: { user }, relations: ['movies'] });
+  async getListsByUser(userId: number): Promise<List[]> {
+    const user = await this.usersService.getUserById(userId);
+    return await this.listRepository.find({
+      where: { user },
+      relations: ['movies'],
+    });
   }
 
   async getPublicList(listId: number): Promise<List> {
-    const list = await this.listRepository.findOne({ where: { id: listId, isPublic: true }, relations: ['movies'] });
+    const list = await this.listRepository.findOne({
+      where: { id: listId, isPublic: true },
+      relations: ['movies'],
+    });
 
     if (!list) {
       throw new NotFoundException(`Public list with id "${listId}" not found`);
