f9ffe5498fc8ea870de63465255972298fdd34a7
niketjain1 <niketj2000@gmail.com>
2024-06-02 11:41:36 +0530

    📦 NEW: Added cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const allowedOrigins =
+    configService.get<string>('BACKEND_PUBLIC_URL') || 'http://localhost:3001';
+
+  app.enableCors({
+    origin: allowedOrigins.split(','),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
