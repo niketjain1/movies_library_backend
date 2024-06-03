ac2ffb556b1780ccc5cc9292812e6aac067df6d9
niketjain1 <niketj2000@gmail.com>
2024-06-03 19:29:45 +0530

    👌 IMPROVE: Added ssl

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,27 +1,31 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Movie } from "src/movie/movie.entity";
-import { List } from "src/playlist/list.enitity";
-import { User } from "src/users/users.entity";
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from 'src/movie/movie.entity';
+import { List } from 'src/playlist/list.enitity';
+import { User } from 'src/users/users.entity';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                type: 'postgres',
-                host: configService.get('DB_HOST'),
-                port: configService.get('DB_PORT'),
-                username: configService.get('DB_USERNAME'),
-                password: configService.get('DB_PASSWORD'),
-                database: configService.get('DB_NAME'),
-                entities: [User, List, Movie],
-                synchronize: true
-            })
-        })
-    ]
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User, List, Movie],
+        synchronize: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: configService.get('DB_SSL') || false,
+          },
+        },
+      }),
+    }),
+  ],
 })
-
 export class DatabaseModule {}
